870135331efde2c6123ae81fa89d2a4c51b0050b
Monteso Alexandrov <montesoalexandrov@gmail.com>
2018-07-02 15:22:57 +0300

    Update JS Advanced repo

---
+++ JS Advanced/14. Lab - Prototype Chain/01. Person and Teacher.js
@@ -0,0 +1,22 @@
+function personAndTeacher() {
+    class Person {
+        constructor(name, email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+    class Teacher extends Person {
+        constructor(name, email, subject) {
+            super(name, email);
+            this.subject = subject;
+        }
+    }
+
+    return {Person, Teacher};
+}
+
+let obj = personAndTeacher();
+let Teacher = obj.Teacher;
+let t = new Teacher('Pesho', 'pesho@abv.bg', 'Javascript');
+console.log(t);
