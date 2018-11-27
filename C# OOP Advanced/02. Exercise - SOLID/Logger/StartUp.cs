54a3433ccac06b250f4c522157de3adf9c31c428
montesoalexandrov <montesoalexandrov@gmail.com>
2018-11-27 15:51:24 +0200

    Added Exercise - SOLID.

---
+++ C# OOP Advanced/02. Exercise - SOLID/Logger/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Logger
+{
+    using System;
+
+    public class StartUp
+    {
+
+        static void Main(string[] args)
+        {
+            int countOfAppenders = int.Parse(Console.ReadLine());
+            var controller = new Controller();
+            controller.Act(countOfAppenders);
+        }
+    }
+}
