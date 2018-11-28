d4859f8e009145f098c2f3ee6846afaa99fee52f
montesoalexandrov <montesoalexandrov@gmail.com>
2018-11-28 20:20:27 +0200

    Added Lab - Generics.

---
+++ C# OOP Advanced/03. Lab - Generics/ArrayCreator/ArrayCreator.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericArrayCreator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T element)
+        {
+            var array = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = element;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ C# OOP Advanced/03. Lab - Generics/Box/Box.cs
@@ -0,0 +1,35 @@
+﻿namespace BoxOfT
+{
+    using System.Collections.Generic;
+
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            this.items = new List<T>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.items.Count;
+            }
+        }
+
+        public void Add(T item)
+        {
+            this.items.Add(item);
+        }
+
+        public T Remove()
+        {
+            var item = this.items[items.Count - 1];
+            this.items.RemoveAt(items.Count - 1);
+
+            return item;
+        }
+    }
+}
